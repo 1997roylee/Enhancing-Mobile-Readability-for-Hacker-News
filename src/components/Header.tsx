770293045e5fb8263046b70c46f7c2230cfacd21
0xRoy <1997roylee@gmail.com>
2023-12-30 19:36:34 +0800

    Refactor Header component to include a logo and a button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,30 @@
-import { Flex } from '@radix-ui/themes';
+import { Box, Button, Flex } from '@radix-ui/themes';
 import Image from 'next/image';
 import HackerNewsCategoryList from './HackerNews/HackerNewsCategoryList';
 
 export default function Header() {
     return (
-        <Flex direction={'column'}>
-            <Image
-                src='https://news.ycombinator.com/y18.svg'
-                width={128}
-                height={128}
-                alt='Y18'
-            />
+        <>
+            <Flex
+                className='border-b'
+                p='4'
+                align={'center'}
+                justify={'between'}
+            >
+                <Box>
+                    <Image
+                        src='https://news.ycombinator.com/y18.svg'
+                        width={48}
+                        height={48}
+                        alt='Y18'
+                        className='rounded-full'
+                    />
+                </Box>
+                <Box>
+                    <Button color='orange'>Star Github</Button>
+                </Box>
+            </Flex>
             <HackerNewsCategoryList />
-        </Flex>
+        </>
     );
 }
