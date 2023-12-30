604eae67a4d632ab07f0da0771df90cc45b7e943
0xRoy <1997roylee@gmail.com>
2023-12-30 19:36:49 +0800

    Refactor HackerNewsPost and HackerNewsPostList components

--- src/components/HackerNews/HackerNewsPost.tsx
+++ src/components/HackerNews/HackerNewsPost.tsx
@@ -1,21 +1,57 @@
 import { HackerNewsPost as THackerNewsPost } from '@/lib/y18/types';
-import { Box, Flex } from '@radix-ui/themes';
+import { Box, Flex, Text } from '@radix-ui/themes';
 import dayjs from 'dayjs';
+import Link from 'next/link';
+import Avatar from '../Avatar';
+import { RiArrowUpSFill } from 'react-icons/ri';
 
 export interface HackerNewsPostProps {
     post: THackerNewsPost | null;
 }
 
 export default function HackerNewsPost({ post }: HackerNewsPostProps) {
-    console.log(post);
     const date = dayjs.unix(post?.time ?? 0).format('YYYY-MM-DD HH:mm:ss');
+
+    if (!post) return null;
+
+    console.log(post, 'post');
+
     return (
-        <Flex>
-            <Box>{post?.by}</Box>
-            <Box>{post?.title}</Box>
-            <Box> {date}</Box>
-            <Box> {post?.type}</Box>
-            <Box> {post?.url}</Box>
-        </Flex>
+        <Link href={post.url ?? '#'}>
+            <Flex
+                py={'4'}
+                px='3'
+                className='hover:bg-gray-50 relative cursor-pointer'
+                data-post-type={post.type}
+                justify={'between'}
+            >
+                <Flex align={'center'}>
+                    <Avatar name={post.by} />
+                    <Box ml='3' className='flex-1'>
+                        <Text weight={'medium'}>{post?.title}</Text>
+                        <Flex>
+                            <Text size='2' className='text-gray-700'>
+                                {' '}
+                                {post.by}
+                            </Text>
+                            <Text ml='2' size='2' className='text-gray-700'>
+                                {date}
+                            </Text>
+                        </Flex>
+                    </Box>
+                </Flex>
+                <Flex width='9' className='border-l' direction={'column'}>
+                    <Flex
+                        className='flex-1'
+                        align={'center'}
+                        justify={'center'}
+                        direction={'column'}
+                    >
+                        <RiArrowUpSFill size='32px' />
+                        <Text align='center'>{post.score}</Text>
+                    </Flex>
+                </Flex>
+            </Flex>
+        </Link>
     );
 }
--- src/components/HackerNews/HackerNewsPostList.tsx
+++ src/components/HackerNews/HackerNewsPostList.tsx
@@ -1,18 +1,77 @@
 import { type HackerNewsPost as THackerNewsPost } from '@/lib/y18/types';
-import { Flex } from '@radix-ui/themes';
+import { Box, Flex, Text } from '@radix-ui/themes';
 import HackerNewsPost from './HackerNewsPost';
+import HackerNewsLoadMoreButton from './HackerNewsLoadMoreButton';
+import HackerNewsLoadingList from './HackerNewsLoadingList';
+import { useMemo } from 'react';
+import dayjs from 'dayjs';
 
 export interface HackerNewsPostListProps {
     posts: (THackerNewsPost | null)[];
 }
 
 export default function HackerNewsPostList({ posts }: HackerNewsPostListProps) {
-    console.log('posts', posts);
+    // const postsByDate = useMemo(() => {
+    //     const grouped = posts.reduce(
+    //         (groups, post) => {
+    //             if (post) {
+    //                 const date = dayjs
+    //                     .unix(post?.time ?? 0)
+    //                     .format('YYYY-MM-DD');
+    //                 if (!groups[date]) {
+    //                     groups[date] = [];
+    //                 }
+    //                 groups[date].push(post);
+    //             }
+    //             return groups;
+    //         },
+    //         {} as Record<string, THackerNewsPost[]>,
+    //     );
+    //     return grouped;
+    // }, [posts]);
+
+    const postsByDate = posts.reduce(
+        (groups, post) => {
+            if (post) {
+                const date = dayjs.unix(post?.time ?? 0).format('YYYY-MM-DD');
+                if (!groups[date]) {
+                    groups[date] = [];
+                }
+                groups[date].push(post);
+            }
+            return groups;
+        },
+        {} as Record<string, THackerNewsPost[]>,
+    );
+
     return (
         <Flex direction={'column'}>
-            {posts.map((post, index) => {
-                return <HackerNewsPost key={post?.id ?? index} post={post} />;
+            {Object.entries(postsByDate).map(([date, posts]) => {
+                return (
+                    <div key={date}>
+                        <Box className='sticky top-0 z-10'>
+                            <Box className='bg-white p-4 border-b'>
+                                <Text size={'6'}>
+                                    {date}
+                                    {/* Top Products Launching Today */}
+                                </Text>
+                            </Box>
+                            <div className='sticky_header_bottom' />
+                        </Box>
+                        <HackerNewsLoadingList />
+                        {posts.map((post, index) => {
+                            return (
+                                <HackerNewsPost
+                                    key={post?.id ?? index}
+                                    post={post}
+                                />
+                            );
+                        })}
+                    </div>
+                );
             })}
+
+            <HackerNewsLoadMoreButton />
         </Flex>
     );
 }
