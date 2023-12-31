2de635e2922cbe699499ba065bc22485ae702acf
0xRoy <1997roylee@gmail.com>
2023-12-31 19:22:58 +0800

    Refactor RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,29 +4,20 @@ import Providers from '@/components/Providers';
 import Header from '@/components/Header';
 import { Container } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
-import HackerNewsCategoryList from '@/components/HackerNews/HackerNewsCategoryList';
-import { Suspense } from 'react';
-import Loading from './loading';
+import { PropsWithChildren } from 'react';
 
 export const metadata: Metadata = {
     title: 'Hacker News',
     description: 'Hacker News',
 };
 
-export default function RootLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: PropsWithChildren) {
     return (
         <html lang='en'>
             <body>
                 <Providers>
                     <Header />
-                    <Container>
-                        <HackerNewsCategoryList />
-                        <Suspense fallback={<Loading />}>{children}</Suspense>
-                    </Container>
+                    <Container>{children}</Container>
                 </Providers>
             </body>
         </html>
