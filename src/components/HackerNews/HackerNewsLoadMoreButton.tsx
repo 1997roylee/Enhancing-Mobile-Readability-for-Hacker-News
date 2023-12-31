301328a881f1f982f0d75a2d0a1f6086d6aa7087
0xRoy <1997roylee@gmail.com>
2023-12-31 17:22:05 +0800

    Refactor HackerNewsLoadMoreButton component

--- src/components/HackerNews/HackerNewsLoadMoreButton.tsx
+++ src/components/HackerNews/HackerNewsLoadMoreButton.tsx
@@ -1,15 +1,27 @@
 'use client';
 
-import { revalidateHome } from '@/lib/pages';
+import { Button } from '@radix-ui/themes';
+// import { revalidateHome } from '@/lib/pages';
+import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 
 export default function HackerNewsLoadMoreButton() {
     const searchParams = useSearchParams();
+    // const router = useRouter();
     const n = searchParams.get('n') ? Number(searchParams.get('n')) + 30 : 30;
 
-    const handleLoad = async () => {
-        revalidateHome(n);
-    };
+    // const handleLoad = async () => {
+    //     revalidateHome(n);
 
-    return <button onClick={handleLoad}>Load more</button>;
+    //     router.push(`?n=${n}`, {
+    //         scroll: false,
+    //     });
+    // };
+
+    return (
+        <Link href={`?n=${n}`} shallow scroll={false}>
+            <Button variant='outline'>Load more</Button>
+        </Link>
+    );
 }
