34c1fd6180adc389a7fcbcc9f3a9d2d4374fb19b
0xRoy <1997roylee@gmail.com>
2023-12-31 17:20:51 +0800

    Add server.ts file for fetching news items

---
+++ src/lib/y18/server.ts
@@ -0,0 +1,56 @@
+'use server';
+
+import { CATEGORY_LIST } from '@/utils/category';
+import {
+    REVALIDATE_TIME,
+    createAPIEndpoint,
+    isOKResponse,
+    returnError,
+    returnSuccess,
+} from '.';
+import { type HackerNewsPost } from './types';
+
+export const getNews = async (categoryLabel: string) => {
+    const category =
+        CATEGORY_LIST.find((item) => item.label === categoryLabel) ??
+        CATEGORY_LIST[0];
+
+    const response = await fetch(
+        createAPIEndpoint(`/v0/${category.path}.json?print=pretty`),
+        { next: { revalidate: REVALIDATE_TIME, tags: [category.path] } },
+    );
+
+    const data = await response.json();
+
+    if (isOKResponse(response)) {
+        return returnSuccess<number[]>(data);
+    }
+
+    return returnError(new Error('Error fetching newest news'));
+};
+
+export const getNewestNewsItems = async (postIds: number[]) => {
+    const news = await Promise.all(
+        postIds.map(async (id) => {
+            const response = await fetch(
+                createAPIEndpoint(`/v0/item/${id}.json?print=pretty`),
+                {
+                    next: {
+                        revalidate: REVALIDATE_TIME,
+                        tags: ['item', id.toString()],
+                    },
+                },
+            );
+
+            const data = await response.json();
+
+            if (isOKResponse(response)) {
+                return data as HackerNewsPost;
+            }
+
+            return null;
+        }),
+    );
+
+    return returnSuccess<(HackerNewsPost | null)[]>(news);
+};
