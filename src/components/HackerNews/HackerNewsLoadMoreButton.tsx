4df819827a8952b66d1cc06176d73ba069f81005
0xRoy <1997roylee@gmail.com>
2024-01-01 20:33:37 +0800

    feat: enabled infinite scroll

--- src/components/HackerNews/HackerNewsLoadMoreButton.tsx
+++ src/components/HackerNews/HackerNewsLoadMoreButton.tsx
@@ -1,39 +1,57 @@
 'use client';
 
-import { Button, Flex } from '@radix-ui/themes';
-import { useState } from 'react';
+import { Flex, Text } from '@radix-ui/themes';
+import { useCallback, useEffect, useState } from 'react';
 import HackerNewsLoadingList from './HackerNewsLoadingList';
 import { fetchNews } from '@/app/actions';
 import { HackerNewsPost } from '@/lib/y18/types';
+import { useInView } from 'react-intersection-observer';
 
 export type HackerNewsLoadMoreButtonProps = {
     category?: string;
+    enabled?: boolean;
     onLoadMore?: (posts: HackerNewsPost[]) => void;
 };
 
 export default function HackerNewsLoadMoreButton({
     onLoadMore,
     category = 'top',
+    enabled,
 }: HackerNewsLoadMoreButtonProps) {
-    const [page, setPage] = useState(0);
+    const [page, setPage] = useState(1);
     const [isLoading, setIsLoading] = useState(false);
 
-    const handleLoadMore = async () => {
+    const { ref, inView, entry } = useInView({
+        threshold: 0.3,
+        delay: 300,
+    });
+
+    const handleLoadMore = useCallback(async () => {
         setIsLoading(true);
         const posts = await fetchNews(category, page + 1);
         setPage((_page) => _page + 1);
         onLoadMore?.(posts as HackerNewsPost[]);
-        setIsLoading(false);
-    };
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 300);
+    }, [category, onLoadMore, page]);
+
+    useEffect(() => {
+        if (inView && isLoading === false) {
+            handleLoadMore();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [inView, isLoading]);
 
     return (
         <>
             {isLoading && <HackerNewsLoadingList />}
-            <Flex justify={'center'} width='100%' mb={'6'}>
-                <Button variant='outline' onClick={handleLoadMore}>
-                    Load more
-                </Button>
-            </Flex>
+
+            {enabled && (
+                <Flex justify={'center'} width='100%' mb={'6'} ref={ref}>
+                    <Text>Loading more...</Text>
+                </Flex>
+            )}
         </>
     );
 }
