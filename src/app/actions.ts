1ed581760d404c05a00b246d3334cfbf1d36d0da
0xRoy <1997roylee@gmail.com>
2024-01-01 20:34:05 +0800

    bugfix: display posts

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,21 +1,27 @@
 'use server';
 
 import { getNews, getNewsItems } from '@/lib/y18/server';
-// import { revalidatePath } from 'next/cache';
 import { notFound } from 'next/navigation';
 
 export async function fetchNews(category: string, page: number) {
     const news = await getNews(category);
     const postIds = news.success ? news.data : [];
 
-    console.log('fetchNews', category);
     if (Number(page * 20) > postIds.length) {
         notFound();
     }
 
-    const postsResult = await getNewsItems(postIds.slice(0, Number(page * 20)));
+    const _postIds = postIds.slice(Number((page - 1) * 20), Number(page * 20));
+
+    const postsResult = await getNewsItems(_postIds);
     const posts = postsResult.success ? postsResult.data : [];
-    // revalidatePath(`/${category}`, 'page');
+
+    console.log(
+        'postsResult',
+        posts.map((post) => post.id),
+        _postIds,
+        postIds.length,
+    );
 
     return posts;
 }
