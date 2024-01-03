3563578060556158003199b4d0d49368b7984082
0xRoy <1997roylee@gmail.com>
2024-01-04 01:30:55 +0800

    update post ui for testing

--- src/components/HackerNews/HackerNewsPost.tsx
+++ src/components/HackerNews/HackerNewsPost.tsx
@@ -1,13 +1,13 @@
 import { HackerNewsPost as THackerNewsPost } from '@/lib/y18/types';
 import dayjs from 'dayjs';
 import Link from 'next/link';
-import { RiArrowUpSFill, RiTimer2Line } from 'react-icons/ri';
 import { formatRelative } from '@/utils/date';
 import Flex from '../ui/Flex';
 import Text from '../ui/Text';
 import { useCallback } from 'react';
 import { useReads } from '@/store/useReads';
 import HackerNewsPostStatus from './HackerNewsPostStatus';
+import { IoTimeOutline, IoCaretUpSharp, IoChatbubbleOutline } from 'react-icons/io5';
 
 export interface HackerNewsPostProps {
     post: THackerNewsPost;
@@ -40,75 +40,68 @@ export default function HackerNewsPost({ post }: HackerNewsPostProps) {
     return (
         <div data-id={post.id}>
             <Flex
-                className='hover:bg-gray-50 relative py-1 px-3'
+                className='hover:bg-gray-50 relative py-1 mx-3 border-b pb-1'
                 data-post-type={post.type}
                 justify={'between'}
             >
                 <Flex className='flex-1 pr-1 items-center'>
                     <div className='flex-1'>
-                        <Flex className='items-start'>
+                        <Flex>
                             <Flex className='items-center justify-center flex-col cursor-pointer w-6'>
-                                <Flex
-                                    className='flex-col items-center justify-center'
-                                    onClick={handleVote}
-                                >
-                                    <RiArrowUpSFill size='24px' className='text-slate-700' />
-                                    <Text className='text-slate-700 text-xs text-center'>
+                                <Flex className='flex-col justify-center mb-2' onClick={handleVote}>
+                                    <IoCaretUpSharp size='18px' className='text-slate-700' />
+                                    <Text className='text-slate-700 text-xs text-center font-light'>
                                         {post.score}
                                     </Text>
                                 </Flex>
-                                <HackerNewsPostStatus postId={post.id} />
-                            </Flex>
-
-                            <div className='ml-2 flex-1'>
-                                <div
-                                    className='cursor-pointer'
-                                    onClick={handleOnNavigate(post.url)}
+                                <Flex
+                                    className='flex-col items-center justify-center hover:underline cursor-pointer'
+                                    onClick={handleOnNavigate(
+                                        `https://news.ycombinator.com/item?id=${post.id}`,
+                                    )}
                                 >
-                                    <Text className='text-sm'>{post?.title}</Text>
-                                    <div className='text-gray-500 text-2xs'>{domain}</div>
-                                </div>
-
-                                <Flex className='items-end mt-1'>
-                                    <Flex className='items-center'>
-                                        <RiTimer2Line className='text-slate-600' size='12px' />
-                                        <Text className='text-slate-600 text-xs font-light ml-1'>
-                                            {date}
-                                        </Text>
-                                        {/* <Link
-                                            href={`https://news.ycombinator.com/item?id=${post.id}`}
-                                            className='inline-block'
-                                        >
-                                            <Text className='text-slate-600 hover:underline text-xs font-light ml-1'>
-                                                {date}
-                                            </Text>
-                                        </Link> */}
-                                    </Flex>
-
-                                    <Text className='ml-1 text-slate-600 hover:underline text-xs font-light'>
-                                        <Link
-                                            href={`https://news.ycombinator.com/user?id=` + post.by}
-                                        >
-                                            <Text className='text-black text-xs'>By</Text> {post.by}
-                                        </Link>
+                                    <IoChatbubbleOutline size='16px' className='text-slate-700' />
+                                    <Text className='text-slate-700 text-center text-xs font-light'>
+                                        {post.descendants}
                                     </Text>
                                 </Flex>
-                            </div>
+                            </Flex>
+
+                            <Flex className='ml-2 flex-1 flex-col'>
+                                <Flex className='flex-col flex-1 justify-between'>
+                                    <div
+                                        className='cursor-pointer'
+                                        onClick={handleOnNavigate(post.url)}
+                                    >
+                                        <Text className='text-sm'>{post?.title}</Text>
+                                        <div className='text-gray-500 text-2xs'>{domain}</div>
+                                    </div>
+
+                                    <Flex className='items-end mt-1 '>
+                                        <HackerNewsPostStatus postId={post.id} />
+                                        <Flex className='items-center'>
+                                            <IoTimeOutline className='text-slate-600' size='12px' />
+                                            <Text className='text-slate-600 text-xs font-light ml-1'>
+                                                {date}
+                                            </Text>
+                                        </Flex>
+
+                                        <Text className='ml-1 text-slate-600 hover:underline text-xs font-light'>
+                                            <Link
+                                                href={
+                                                    `https://news.ycombinator.com/user?id=` +
+                                                    post.by
+                                                }
+                                            >
+                                                <Text className='text-black text-xs'>By</Text>{' '}
+                                                {post.by}
+                                            </Link>
+                                        </Text>
+                                    </Flex>
+                                </Flex>
+                            </Flex>
                         </Flex>
                     </div>
-                </Flex>
-                <Flex className='flex-col justify-center'>
-                    <Flex
-                        className='flex-col items-center justify-center flex-1 hover:underline cursor-pointer'
-                        onClick={handleOnNavigate(
-                            `https://news.ycombinator.com/item?id=${post.id}`,
-                        )}
-                    >
-                        <Text className='text-slate-600 text-center text-sm font-light'>
-                            {post.descendants}
-                        </Text>
-                        <Text className='text-2xs text-slate-500 font-light'>comments</Text>
-                    </Flex>
                 </Flex>
             </Flex>
         </div>
--- src/components/HackerNews/HackerNewsPostStatus.tsx
+++ src/components/HackerNews/HackerNewsPostStatus.tsx
@@ -9,7 +9,7 @@ export default function HackerNewsPostStatus({ postId }: { postId: number }) {
     const isRead = getIsRead(postId);
 
     if (isRead) {
-        return <Text className='text-green-700 text-xs text-center'>Read</Text>;
+        return <Text className='text-green-700 text-xs text-center mr-1'>Read</Text>;
     }
 
     return null;
